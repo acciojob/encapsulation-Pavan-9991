6f96cfe86fd0b952bd91770ad39e42cc77e2e7a2
Pavan <ysaipavankumar9991@gmail.com>
2024-04-05 15:53:13 +0530

    The encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static void main(String [] args){
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Pavan Kumar");
+
+        System.out.println(obj.getName());
+
+
+    }
+
+    public static class RWOnly{
+        private String name;
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
   
 }
